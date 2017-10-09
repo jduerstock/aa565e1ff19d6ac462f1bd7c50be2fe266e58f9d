93a8dd3bb3e979d5b90985a2ebb64b9af5eeb9d6
Jason Duerstock <jason.duerstock@gmail.com>
2017-10-09 10:25:47 -0400

    tick

--- make.sh
+++ make.sh
@@ -4,3 +4,6 @@ M68KLD=m68k-linux-gnu-ld
 $M68KAS -g -o cfm_0000.o cfm_0000.s || exit
 $M68KLD --oformat=binary -Ttext=0x10000000 -e 0x10000000 -o cfm_0000.bin cfm_0000.o || exit
 sha1sum -c cfm_0000.sha1
+$M68KAS -g -o boot0003.o boot0003.s || exit
+$M68KLD --oformat=binary -Ttext=0x10000000 -e 0x10000000 -o boot0003.bin boot0003.o || exit
+sha1sum -c boot0003.sha1
